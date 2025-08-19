1175eaa598ca36c54abc6e9a4d06bf143b75148d
kadirkara <kadirkar2204@gmail.com>
2025-08-20 00:12:12 +0300

    Add RabbitMQ integration for ActivityService

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -4,20 +4,32 @@ import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
 
-    public ActivityService(ActivityRepository activityRepository, UserValidationService userValidationService) {
+    @Value("${rabbitmq.exchange.name}")
+    private  String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    public ActivityService(ActivityRepository activityRepository, UserValidationService userValidationService, RabbitTemplate rabbitTemplate) {
         this.activityRepository = activityRepository;
         this.userValidationService = userValidationService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
 
@@ -36,6 +48,13 @@ public class ActivityService {
                 .build();
 
         Activity saveActivity=activityRepository.save(activity);
+
+        try{
+              rabbitTemplate.convertAndSend(exchange,routingKey,saveActivity);
+        } catch (Exception e) {
+            log.error("Failed to publish activity to RabbitMQ : "  , e);
+        }
+
         return mapToResponse(saveActivity);
 
 

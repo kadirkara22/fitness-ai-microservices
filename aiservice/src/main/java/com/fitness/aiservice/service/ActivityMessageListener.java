d776d07a7aa883f01d0c4e83fd7f5f85f4adae28
kadirkara <kadirkar2204@gmail.com>
2025-08-20 23:05:14 +0300

    Add gemini api

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +12,24 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ActivityMessageListener {
 
+    private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
+
+    public ActivityMessageListener(ActivityAIService aiService, RecommendationRepository recommendationRepository) {
+        this.aiService = aiService;
+        this.recommendationRepository = recommendationRepository;
+    }
     @Value("${rabbitmq.queue.name}")
     private  String queueName;
 
     @RabbitListener(queues ="activity.queue" )
     public void processActivity(Activity activity){
         log.info("Received activity for processing: {}", activity.getId());
+        //log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+
+        Recommendation recommendation =aiService.generateRecommendation(activity);
+
+        recommendationRepository.save(recommendation);
     }
 
 }
